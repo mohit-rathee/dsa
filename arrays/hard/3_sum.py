82208177c7111a535716800de12cec8f509d6f3a
mohit-rathee <mohit.rathee2505@gmail.com>
2026-06-11 17:04:46 +0530

    3 sum with both hashset and 2 pointer fixed

--- arrays/hard/3_sum.py
+++ arrays/hard/3_sum.py
@@ -1,6 +1,6 @@
 # skip duplicate elements
 def update_var(arr, idx, val, inc=True):
-    while arr[idx] == val and idx < len(arr):
+    while 0 <= idx < len(arr) and arr[idx] == val:
         if inc:
             idx += 1
         else:
@@ -26,12 +26,12 @@ def three_sum(arr, sum):
         k = len(arr)-1
 
         desired_num = sum-i_val
-        print('for', i_val, 'desired_num', desired_num)
+        # print('for', i_val, 'desired_num', desired_num)
 
         while j < k:
             j_val = arr[j]
             k_val = arr[k]
-            print(i_val, j_val, k_val)
+            # print(i_val, j_val, k_val)
 
             s = j_val + k_val
             if s < desired_num:
@@ -43,13 +43,36 @@ def three_sum(arr, sum):
             else:
                 # append to result
                 result.append([i_val, j_val, k_val])
-                break
+                j = update_var(arr, j+1, j_val, True)
+                # break
 
         # incrementing i
         i = update_var(arr, i+1, i_val, True)
     print(result)
 
 
+def three_sum_hashmap(arr, sum):
+    result = set()
+    for i in range(len(arr)-2):
+        i_val = arr[i]
+
+        hashset = set()
+
+        for j in range(i+1, len(arr)):
+            j_val = arr[j]
+
+            desired_num = sum - j_val - i_val
+            # print(i_val, j_val, 'needs ', desired_num)
+            if desired_num in hashset:
+                res = sorted([i_val, j_val, desired_num])
+                result.add(tuple(res))
+            hashset.add(j_val)
+    print(list(result))
+
+
 arr = [-1, -1, 0, 1, -4, 2, -1, -4, 2, 2, 2, 3, 5]
-# p = [-1, -1, 0, 2, 1, -3]
-three_sum(arr, 1)
+# arr = [-1, -1, 1, 0, 2, 1, -3]
+
+sum = 1
+three_sum(arr, sum)
+three_sum_hashmap(arr, sum)
