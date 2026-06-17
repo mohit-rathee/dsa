4736d3336a82a56030a00080cee2ba45b3335689
mohit-rathee <mohit.rathee2505@gmail.com>
2026-06-17 16:19:54 +0530

    kth missing element using proper intution

--- binary_search/bs_on_answers/kth_missing_positive_no.py
+++ binary_search/bs_on_answers/kth_missing_positive_no.py
@@ -1,24 +1,18 @@
 def kth_missing_number(vec, K):
     # vec should start from 1 to inf
     print(vec, K)
-    n = len(vec) - 1
+    n = len(vec)
     # check for last value EDGE CASE
-    expected_val = n + 1 + K
-    actual_val = vec[n]
-    if not expected_val <= actual_val:
-        expected_num = n + 1
-        # req_diff = K
-        return expected_num + K
-
     low, high = 0, n
     while low < high:
         mid = (low + high) // 2
         # print(low, mid, high)
 
         # i think kth missing number lies before mid
-        expected_val = mid + 1 + K
+        expected_val = mid + 1
         actual_val = vec[mid]
-        if expected_val <= actual_val:
+        missing_elements = actual_val - expected_val
+        if missing_elements >= K:
             # Kth missing number is on left side
             # print("valid")
             # print(expected_val,actual_val)
@@ -30,17 +24,17 @@ def kth_missing_number(vec, K):
             # print(expected_val,actual_val)
             low = mid + 1
     # return low
+    print("low", low)
+    print("high", high)
 
-    expected_num = low + 1
-    # req_diff = K
-    return expected_num + K - 1
+    return low + K
 
 
-# vec = [4, 7, 9, 10]
-# K = 9
+vec = [4, 7, 9, 10]
+K = 9
 # vec = [4, 7, 9, 10]
 # K = 4
-vec = [1, 2, 3, 5]
-K = 1
+# vec = [1, 2, 4, 6]
+# K = 2
 ans = kth_missing_number(vec, K)
 print(ans)
