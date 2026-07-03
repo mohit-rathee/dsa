3b79e12cfe82d202d07da2cbaf882b24d793d692
mohit-rathee <mohit.rathee2505@gmail.com>
2026-07-03 17:48:54 +0530

    count good numbers

---
+++ recursion/basic/good_number_count.py
@@ -0,0 +1,29 @@
+class Solution:
+    def __init__(self):
+        self.even_count = 5  # 0,2,4,6,8
+        self.prime_count = 4  # 2,3,5,7
+
+    def no_of_possibilities_until(self, i, n):
+        # base case
+        if i >= n:
+            return 1
+
+        if i % 2 == 0:
+            return self.even_count * self.no_of_possibilities_until(i + 1, n)
+        else:
+            return self.prime_count * self.no_of_possibilities_until(i + 1, n)
+
+    def count_good_numbers_brute_force(self, n):
+        return self.no_of_possibilities_until(0, n)
+
+    def count_good_numbers_optimal(self, n):
+        even_indices_count = (n + 1) // 2
+        odd_indices_count = n // 2
+        return self.even_count**even_indices_count * self.prime_count**odd_indices_count
+
+
+sol = Solution()
+ans = sol.count_good_numbers_brute_force(4)
+print(ans)
+ans = sol.count_good_numbers_optimal(4)
+print(ans)
